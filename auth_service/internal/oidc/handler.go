8185783ee2fe8a7b7fcf808104d90621e146fb2a
Hristo Partenov <hristokubev@gmail.com>
2026-06-11 23:01:55 +0300

    fix: small issues

--- auth_service/internal/oidc/handler.go
+++ auth_service/internal/oidc/handler.go
@@ -103,7 +103,7 @@ func (h *handler) Start(w http.ResponseWriter, r *http.Request) {
 func (h *handler) Callback(w http.ResponseWriter, r *http.Request) {
 	log.Println("/callback was hit")
 
-	ctx, cancel := context.WithTimeout(r.Context(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(r.Context(), 1*time.Minute)
 	defer cancel()
 
 	session, err := h.cookieService.ReadOAuthCookie(r)
